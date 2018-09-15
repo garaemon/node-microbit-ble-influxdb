62d83ac27094e7ed178c18c79c9da0ac1efcb825
Ryohei Ueda <garaemon@gmail.com>
2018-09-15 15:33:08 +0900

    Setup systemd

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-rsync -avz ./*.json $1/
-rsync -avz ./build/ $1/build/
+npm run build
+
+host=$(echo "$1" | cut -d: -f1)
+directory=$(echo "$1" | cut -d: -f2)
+rsync -avz ./*.json "$1"/
+rsync -avz ./build/ "$1"/build/
+rsync -avz ./resources/ "$1"/resources/
+
+# register service
+ssh "$host" <<EOF
+source .nvm/nvm.sh
+export NODE_PATH=\$(dirname \$(which node))
+export PROJECT_DIR=\$(readlink -f ${directory})
+mkdir -p \${HOME}/.config/systemd/user/
+envsubst < ./${directory}/resources/microbit.service.template > \${HOME}/.config/systemd/user/microbit.service
+systemctl --user daemon-reload
+systemctl --user enable microbit
+systemctl --user restart microbit
+EOF
